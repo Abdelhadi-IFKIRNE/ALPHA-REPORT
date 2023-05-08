fa604f2a6ee05bc176ccf0aa91f96b35427abf31
Abdelhadi-IFKIRNE <abdelhadi.ifkirne@gmail.com>
2023-05-08 10:28:18 +0000

    update report

---
+++ src/main/java/com/example/alphareport/AlphaReportApplication.java
@@ -0,0 +1,13 @@
+package com.example.alphareport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaReportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlphaReportApplication.class, args);
+    }
+
+}
